363a4edc1b177aa300bf15eed11018f0f7600cbd
hamdi-ben-yaflah <hamdiby@xtech.guru>
2021-12-29 11:11:00 +0100

    add react-router and create choices context

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,53 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { ReactNode, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Layout } from "antd";
 import "./App.css";
+import Score from "./components/Score/Score";
+import Game from "./pages/Game/Game";
+import AutoPlay from "./pages/Auto-play/AutoPlay";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import { Choice, IGameMode, IScore } from "./typings/game";
+import Rock from "./icons/Rock";
+import Paper from "./icons/Paper";
+import Scessior from "./icons/Scessior";
+
+const { Header, Footer, Content } = Layout;
+
+const CHOICES: Choice[] = [
+  { id: 1, name: "rock", component: <Rock />, beats: 3 },
+  { id: 2, name: "paper", component: <Paper />, beats: 1 },
+  { id: 3, name: "scissors", component: <Scessior />, beats: 2 },
+];
+
+export const ChoicesContext = React.createContext<Choice[]>([]);
 
 function App() {
+  const [socre, setScore] = useState<IScore>({ wins: 0, loses: 0 });
+  const [gameMode, setGameMode] = useState<IGameMode>();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      {gameMode !== "not-set" && (
+        <Header>
+          <Score score={socre} />
+        </Header>
+      )}
+      <Content className="main-content">
+        <ChoicesContext.Provider value={CHOICES}>
+          <Router>
+            <Routes>
+              <Route
+                path="/"
+                element={<Dashboard setGameMode={setGameMode} />}
+              />
+              <Route path="/solo" element={<Game setScore={setScore} />} />
+              <Route path="/auto" element={<AutoPlay />} />
+            </Routes>
+          </Router>
+        </ChoicesContext.Provider>
+      </Content>
+      <Footer className="footer">AERQ ©2022 Created by Hamdi Ben Yaflah</Footer>
+    </Layout>
   );
 }
 
