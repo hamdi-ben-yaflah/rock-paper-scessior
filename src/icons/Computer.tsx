65a5739ca5026b6c7924d35b6c04a2d41fa40b6e
hamdi-ben-yaflah <hamdiby@xtech.guru>
2021-12-29 11:08:38 +0100

    add icons

---
+++ src/icons/Computer.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+// eslint-disable-next-line func-names
+export default function () {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      version="1.1"
+      id="Capa_1"
+      x="0px"
+      y="0px"
+      width="100px"
+      height="100px"
+      viewBox="0 0 493.551 493.551"
+    >
+      <g>
+        <g>
+          <path d="M493.549,402.462H0v45.033h493.551v-45.033H493.549z M471.934,430.38h-39.629v-10.809h39.629V430.38L471.934,430.38z" />
+          <polygon points="474.635,338.967 21.616,338.967 0,384 493.549,384   " />
+          <path d="M22.966,321.158h447.62V46.055H22.966V321.158z M72.049,85.195h349.447v196.827H72.049V85.195z" />
+        </g>
+      </g>
+    </svg>
+  );
+}
