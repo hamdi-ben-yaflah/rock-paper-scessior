17cd8fa03f1be2a2c9ea33b6826667af744f51b6
hamdi-ben-yaflah <hamdiby@xtech.guru>
2021-12-29 11:09:33 +0100

    implement dashboard page

---
+++ src/pages/Dashboard/Dashboard.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect } from "react";
+import { Typography, Row, Col, Space } from "antd";
+import { Link } from "react-router-dom";
+import Computer from "../../icons/Computer";
+import Person from "../../icons/Person";
+import styles from "./Dashboard.module.css";
+import { IGameMode } from "../../typings/game";
+
+interface DashboardProps {
+  setGameMode: (gameMode: IGameMode) => void;
+}
+
+const { Title } = Typography;
+
+function Dashboard({ setGameMode }: DashboardProps) {
+  useEffect(() => setGameMode("not-set"), []);
+
+  return (
+    <Space direction="vertical">
+      <Title>Welcome To Rock, Paper, Scessior Game</Title>
+      <Title level={3}>Choose The Game Mode</Title>
+      <Row justify="center">
+        <Col span={11}>
+          <Link
+            to="/solo"
+            className={styles.link}
+            onClick={() => setGameMode("solo")}
+          >
+            <Person /> <Title level={3}> VS </Title> <Computer />
+          </Link>
+          <Title level={5}> You VS Computer </Title>
+        </Col>
+        <Col span={2}>/</Col>
+        <Col span={11}>
+          <Link
+            to="/auto"
+            className={styles.link}
+            onClick={() => setGameMode("auto")}
+          >
+            <Computer /> <Title level={3}> VS </Title> <Computer />
+          </Link>
+          <Title level={5}> Computer VS Computer </Title>
+        </Col>
+      </Row>
+    </Space>
+  );
+}
+
+export default Dashboard;
