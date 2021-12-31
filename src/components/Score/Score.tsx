926ba585832bd67656e4f43db053c7f3d2feb1b1
hamdi-ben-yaflah <hamdiby@xtech.guru>
2021-12-31 19:47:53 +0100

    attach data-cy and data-testid to Row

--- src/components/Score/Score.tsx
+++ src/components/Score/Score.tsx
@@ -13,37 +13,37 @@ interface ScorePrpos {
 
 function Score({ score = { wins: 0, loses: 0 }, setScore }: ScorePrpos) {
   return (
-    // The div is just to assign data attribute to get the element in test
-    // For some components such Row ant design does not offer a props to assign data-*
-    <div data-cy="score-banner" data-testid="score-banner">
-      <Row className={styles.scoreRow}>
-        <Col sm={4} md={4} lg={6} xl={8} xxl={10}>
-          <Title type="success" level={2}>
-            <span data-testid="player-one-score">{score.wins}</span>
-          </Title>
-        </Col>
-        <Col sm={12} md={12} lg={8} xl={4} xxl={4}>
-          <Title type="warning" level={2}>
-            Score
-          </Title>
-        </Col>
-        <Col sm={4} md={4} lg={6} xl={8} xxl={8}>
-          <Title type="danger" level={2}>
-            <span data-testid="player-two-score"> {score.loses}</span>
-          </Title>
-        </Col>
-        <Col sm={4} md={4} lg={4} xl={4} xxl={2}>
-          <Button
-            danger
-            type="primary"
-            shape="round"
-            onClick={() => setScore("not-set")}
-          >
-            Reset The Game
-          </Button>
-        </Col>
-      </Row>
-    </div>
+    <Row
+      className={styles.scoreRow}
+      data-cy="score-banner"
+      data-testid="score-banner"
+    >
+      <Col sm={4} md={4} lg={6} xl={8} xxl={10}>
+        <Title type="success" level={2}>
+          <span data-testid="player-one-score">{score.wins}</span>
+        </Title>
+      </Col>
+      <Col sm={12} md={12} lg={8} xl={4} xxl={4}>
+        <Title type="warning" level={2}>
+          Score
+        </Title>
+      </Col>
+      <Col sm={4} md={4} lg={6} xl={8} xxl={8}>
+        <Title type="danger" level={2}>
+          <span data-testid="player-two-score"> {score.loses}</span>
+        </Title>
+      </Col>
+      <Col sm={4} md={4} lg={4} xl={4} xxl={2}>
+        <Button
+          danger
+          type="primary"
+          shape="round"
+          onClick={() => setScore("not-set")}
+        >
+          Reset The Game
+        </Button>
+      </Col>
+    </Row>
   );
 }
 
