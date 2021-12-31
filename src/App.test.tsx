f25ebaf566ab37def53c6bc14285b5cb337bcf55
hamdi-ben-yaflah <hamdiby@xtech.guru>
2021-12-31 20:06:09 +0100

    configure cypress

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,21 +2,9 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-window.matchMedia =
-  window.matchMedia ||
-  // eslint-disable-next-line func-names
-  function () {
-    return {
-      matches: false,
-      addListener() {},
-      removeListener() {},
-    };
-  };
-
-test("renders learn react link", () => {
+test("renders App component correctly", () => {
   render(<App />);
-  const heading = screen.getByRole("heading", {
-    name: /welcome to rock, paper, scessior game/i,
-  });
-  expect(heading).toBeInTheDocument();
+
+  const footer = screen.getByRole("contentinfo");
+  expect(footer).toBeInTheDocument();
 });
